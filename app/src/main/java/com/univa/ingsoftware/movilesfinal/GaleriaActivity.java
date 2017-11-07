62b473ff8c21962af3bd2f1b942c91a19209d3f8
Checo Galván <checo1000@gmail.com>
2017-11-07 00:38:16 -0600

    Commit inicial

---
+++ app/src/main/java/com/univa/ingsoftware/movilesfinal/GaleriaActivity.java
@@ -0,0 +1,16 @@
+package com.univa.ingsoftware.movilesfinal;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class GaleriaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.galeria_activity);
+    }
+
+}
